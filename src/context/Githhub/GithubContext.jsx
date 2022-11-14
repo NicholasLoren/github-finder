09258da75ab64c5b8983ef59605ad8e5502f2ce0
NicholasLoren <lorennicosir@gmail.com>
2022-11-14 19:10:45 +0300

    Removed semi colons and double quotes

--- src/context/Githhub/GithubContext.jsx
+++ src/context/Githhub/GithubContext.jsx
@@ -1,40 +1,40 @@
-import { createContext, useReducer } from "react";
-import GithubReducer from "./GithubReducers";
+import { createContext, useReducer } from 'react'
+import GithubReducer from './GithubReducers'
 
-const GithubContext = createContext();
-const url = process.env.REACT_APP_GITHUB_URL;
-const token = process.env.REACT_APP_GITHUB_TOKEN;
+const GithubContext = createContext()
+const url = process.env.REACT_APP_GITHUB_URL
+const token = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubContextProvider = ({ children }) => {
   const initialState = {
     users: [],
     isLoading: false,
-  };
-  const [state, dispatch] = useReducer(GithubReducer, initialState);
+  }
+  const [state, dispatch] = useReducer(GithubReducer, initialState)
 
   const fetchUsers = async () => {
-    setLoading();
+    setLoading()
     const response = await fetch(`${url}/users`, {
       headers: {
         Authorization: `token ${token}`,
       },
-    });
+    })
 
-    const data = await response.json();
+    const data = await response.json()
     dispatch({
-      type: "FETCH_USERS",
+      type: 'FETCH_USERS',
       payload: data,
-    });
-  };
+    })
+  }
 
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
   return (
     <GithubContext.Provider
       value={{ users: state.users, isLoading: state.isLoading }}
     >
       {children}
     </GithubContext.Provider>
-  );
-};
+  )
+}
 
-export default GithubContext;
+export default GithubContext
